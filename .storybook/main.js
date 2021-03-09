a23d6a6662fc51fcc41029c7dab6097d8ef9296e
Jonathan Zempel <jzempel@gmail.com>
2021-03-09 14:38:22 -0800

    chore: update storybook to align with zendeskgarden repos (#8)

--- .storybook/main.js
+++ .storybook/main.js
@@ -9,7 +9,7 @@
 
 const { styles } = require('@ckeditor/ckeditor5-dev-utils');
 const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
-const { iconPlugins } = require('./gardenIcons');
+const { iconPlugins } = require('./iconPlugins');
 const { plugins } = require('../postcss.config');
 
 function isCssRule(rule) {
@@ -22,7 +22,10 @@ function isSvgRule(rule) {
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  addons: [
+    { name: '@storybook/addon-essentials', options: { actions: false, controls: false } },
+    '@storybook/addon-a11y'
+  ],
   webpackFinal: config => {
     config.module.rules.forEach(rule => {
       if (isCssRule(rule)) {
