d2d780f36e9e0e3acea90dcd0fc840fef0d4ffd9
george treviranus <geowtrev@gmail.com>
2024-07-09 15:06:30 -0500

    chore: code review

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,9 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import prettierConfig from 'eslint-config-prettier';
 import config from '@zendeskgarden/eslint-config';
 import noticePlugin from '@zendeskgarden/eslint-config/plugins/notice.js';
+import prettierConfig from 'eslint-config-prettier';
 import reactPlugin from '@zendeskgarden/eslint-config/plugins/react.js';
 
 export default [
@@ -15,9 +15,6 @@ export default [
   noticePlugin,
   reactPlugin,
   prettierConfig,
-  {
-    ignores: ['**/node_modules/**', '**/dist/**', '**/public/**', '**/.cache/**', '**/.github/**']
-  },
   {
     rules: {
       'sort-imports': ['error', { allowSeparatedGroups: true }]
